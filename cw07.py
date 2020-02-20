e03932d01f147abcd7254eeaca0c6633a1f0ae3a
Mario de Miguel <57144439+tamalk@users.noreply.github.com>
2020-02-20 19:45:04 +0100

    Update cw07.py

--- cw07.py
+++ cw07.py
@@ -2,8 +2,8 @@ ant = 0
 n = 1
 m = int(input())
 for i in range(m):
-  while (i <= len(m) - 1):
-    print(n, end='-')
-    if (i == len(m)):
-      print(n)
-  ant, n = n, ant+n
+    if (i <= (m-2)):
+        print(str(n), end='-')
+    else:
+        print(n)
+    ant, n = n, ant+n
